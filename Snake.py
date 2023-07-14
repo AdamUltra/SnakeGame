50e6e1ac3c5bd148de98ed302e70286489e31cca
PythJavHacker <adam.eldesoky@gmail.com>
2023-07-14 14:54:23 +0300

    Commit

--- Snake.py
+++ Snake.py
@@ -3,10 +3,14 @@ pygame.init()
 dis = pygame.display.set_mode((400, 300))
 pygame.display.update()
 pygame.display.set_caption('Snake Game by AdamUltra')
+blue = (0, 0, 255)
+red = (255, 0, 0)
 GameOver = False
 while not GameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             GameOver = True
+    pygame.draw.rect(dis, blue, [200, 150, 10, 10])
+    pygame.display.update()
 pygame.quit()
 quit()
