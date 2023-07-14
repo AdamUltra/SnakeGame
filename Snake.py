da33640087b280a65a9ce1c1dc090965914a18ea
PythJavHacker <adam.eldesoky@gmail.com>
2023-07-14 14:47:08 +0300

    Commit

--- Snake.py
+++ Snake.py
@@ -2,5 +2,11 @@ import pygame
 pygame.init()
 dis = pygame.display.set_mode((400, 300))
 pygame.display.update()
+pygame.display.set_caption('Snake Game by AdamUltra')
+GameOver = False
+while not GameOver:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            GameOver= True
 pygame.quit()
 quit()
